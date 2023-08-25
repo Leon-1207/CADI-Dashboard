61222fd331ac1af7a1540794e5e3988aded64853
Leon <leon.mainusch@web.de>
2023-08-25 11:46:06 +0200

    NEW: dev page for profile component

---
+++ pages/profile.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="popup-overlay" style="display: block"></div>
+    <profile class="popup-content open" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
