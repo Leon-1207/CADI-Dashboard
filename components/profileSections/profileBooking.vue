d169fed1ac02deaabe78fd5a0b69f1619b726015
Leon Mainusch <leon.mainusch@web.de>
2023-08-29 10:45:22 +0200

    NEW: booking view

--- components/profileSections/profileBooking.vue
+++ components/profileSections/profileBooking.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        
+  <div class="profile-rows-container large-gap">
+    <div class="label-with-value">
+      <span>Reise:</span>
+      <span>Actioncamp Alpensommer Zell am See 16-17 Jahre</span>
     </div>
+    <div class="label-with-value">
+      <span>Termin:</span>
+      <span>28.07. - 13.08.2023</span>
+    </div>
+    <div class="label-with-value">
+      <span>Abweichendes Reisedatum:</span>
+      <span>2023-07-29 bis false</span>
+    </div>
+    <div class="label-with-value">
+      <span>Unterbringung:</span>
+      <span>Hotel Mehrbettzimmer</span>
+    </div>
+    <div class="label-with-value">
+      <span>Verpflegung:</span>
+      <span>Vollpension</span>
+    </div>
+    <div class="label-with-value">
+      <span>Belegungswunsch:</span>
+      <span>
+        Monika Radowska, bitte ein Zimmer, dass sich nicht auf dem Dachboden und
+        nicht auf der Straßenseite befindet. gerne noch mit zwei anderen Mädels
+        im gleichen Alter.
+      </span>
+    </div>
+    <div class="label-with-value">
+      <span>Belegungswunsch:</span>
+      <span>
+        Wir möchten, dass unsere beiden Töchter (Anna und Monika) zusammen in
+        einem Raum sind, der sich nicht auf dem Dachboden und nicht auf der
+        Straßenseite befindet. Die Töchter sprechen Deutsch und würden gerne mit
+        zwei Mädchen aus dem deutschsprachigen Raum
+      </span>
+    </div>
+  </div>
 </template>
