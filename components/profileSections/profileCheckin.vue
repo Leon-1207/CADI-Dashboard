bcbb5a3df0d85c7df1e864cc01cd95e1c9f4aa55
Leon Mainusch <leon.mainusch@web.de>
2023-08-30 20:44:58 +0200

    NEW: check-in content

--- components/profileSections/profileCheckin.vue
+++ components/profileSections/profileCheckin.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>
-        
+  <div class="profile-rows-container">
+    <div class="label-with-value">
+      <span>Dauermedikation:</span>
+      <span>-</span>
     </div>
+    <div class="label-with-value">
+      <span>Allergien:</span>
+      <span>-</span>
+    </div>
+    <div class="label-with-value">
+      <span>Krankheiten und Störungen:</span>
+      <span>-</span>
+    </div>
+    <div class="label-with-value">
+      <span>Unverträglichkeit von Medikamenten:</span>
+      <span>-</span>
+    </div>
+    <div class="label-with-value">
+      <span>Schwimmlevel:</span>
+      <span>Silber (Guter Schwimmer)</span>
+    </div>
+    <div class="label-with-value">
+      <span>Notiz an Betreuer:</span>
+      <span>-</span>
+    </div>
+    <div class="label-with-value">
+      <span>Krankenversicherung:</span>
+      <span>Europäische Krankenversicherungskarte</span>
+    </div>
+    <div class="label-with-value">
+      <span>Versicherungsnehmer:</span>
+      <span>Anna Radowska (Identifikationsnummer 06211104849)</span>
+    </div>
+    <div class="label-with-value">
+      <span>Versicherungsnummer:</span>
+      <span>80616000150027987785</span>
+    </div>
+  </div>
 </template>
