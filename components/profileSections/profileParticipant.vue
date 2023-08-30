7030efbb6506fef2c864c94717930f7be65ae701
Leon Mainusch <leon.mainusch@web.de>
2023-08-30 09:51:32 +0200

    NEW: toggle input

--- components/profileSections/profileParticipant.vue
+++ components/profileSections/profileParticipant.vue
@@ -7,5 +7,13 @@
     <div class="profile-row">
       <basic-input type="date" label="Geburtsdatum"></basic-input>
     </div>
+    <div></div>
+    <label class="checkbox_item citem_2">
+      <label class="checkbox_wrap">
+        <input type="checkbox" name="checkbox" class="checkbox_inp" />
+        <span class="checkbox_mark"></span>
+      </label>
+      <span class="checkbox-label">Aktuell krank</span>
+    </label>
   </div>
 </template>
