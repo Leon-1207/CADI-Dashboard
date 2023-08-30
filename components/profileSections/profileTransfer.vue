86d5c092713da242f7e307caa0c8c6c0cc41b27b
Leon Mainusch <leon.mainusch@web.de>
2023-08-30 21:53:53 +0200

    NEW: transfer profile content

--- components/profileSections/profileTransfer.vue
+++ components/profileSections/profileTransfer.vue
@@ -1,5 +1,78 @@
 <template>
-    <div>
-        
+  <div class="profile-rows">
+    <!-- arrival -->
+    <span class="profile-content-headline">Anreise</span>
+    <div class="label-with-value">
+      <span>Gebuchte Anreise:</span>
+      <span>Eigenverantwortliche Anreise</span>
     </div>
+    <div class="label-with-value">
+      <span>Abweichendes Reisedatum:</span>
+      <span>29.07.2023&ensp;-&ensp;13 Uhr</span>
+    </div>
+    <div class="label-with-value">
+      <span>Ansprechpartner:</span>
+      <div class="content-side-by-side">
+        <span>Caro</span>
+        <a href="tel:+49 1573 6450844" class="link">+49 1573 6450844</a>
+      </div>
+    </div>
+    <div class="label-with-value">
+      <span>Ziel Infos:</span>
+      <div class="profile-rows very-small-gap">
+        <span
+          >Zell am See - Club Kitzsteinhorn, Gletschermoosstraße 16, 5700 Zell
+          am See, 09:00 Uhr</span
+        >
+        <a
+          target="_blank"
+          class="link"
+          href="https://maps.google.com/maps?q=47.2965,12.7904&amp;z=14&amp;basemap=satellite"
+        >
+          In Google Maps öffnen&nbsp;<i class="fas fa-arrow-right" />
+        </a>
+      </div>
+    </div>
+
+    <!-- gap between arrival and departure -->
+    <div class="h-line"></div>
+
+    <!-- departure -->
+    <span class="profile-content-headline">Abreise</span>
+    <div class="label-with-value">
+      <span>Gebuchte Anreise:</span>
+      <span>Eigenverantwortliche Anreise</span>
+    </div>
+    <div class="label-with-value">
+      <span>Abholzeit:</span>
+      <span>12.08.2023&ensp;-&ensp;19 Uhr</span>
+    </div>
+    <div class="label-with-value">
+      <span>Abweichendes Reisedatum:</span>
+      <span>12.08.2023</span>
+    </div>
+    <div class="label-with-value">
+      <span>Ansprechpartner:</span>
+      <div class="content-side-by-side">
+        <span>Caro</span>
+        <a href="tel:+49 1573 6450844" class="link">+49 1573 6450844</a>
+      </div>
+    </div>
+    <div class="label-with-value">
+      <span>Abreise Infos:</span>
+      <div class="profile-rows very-small-gap">
+        <span
+          >Zell am See - Club Kitzsteinhorn, Gletschermoosstraße 16, 5700 Zell
+          am See, 12.08.2023 - 19:00 Uhr</span
+        >
+        <a
+          target="_blank"
+          class="link"
+          href="https://maps.google.com/maps?q=47.2965,12.7904&amp;z=14&amp;basemap=satellite"
+        >
+          In Google Maps öffnen&nbsp;<i class="fas fa-arrow-right" />
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
