5a9a57e241efeede7cf39fdbad65332ce5c50115
Leon Mainusch <leon.mainusch@web.de>
2023-08-30 12:35:57 +0200

    NEW: contacts content

--- components/profileSections/profileContacts.vue
+++ components/profileSections/profileContacts.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        
+  <div class="profile-rows-container">
+    <div class="label-with-value">
+      <span>Erziehungsberechtigter:</span>
+      <span>Max Mustermann</span>
     </div>
+
+    <div class="profile-row extend-to-2-columns-420px">
+      <div class="label-with-value">
+        <span>Mobil:</span>
+        <div>
+          <a class="link" href="tel:+4368181246640">+4368181246640</a>
+          <a
+            class="basic-button"
+            href="https://wa.me/XXXXXXXXXXX"
+            target="_blank"
+          >
+            <i class="fab fa-whatsapp"></i>
+          </a>
+        </div>
+      </div>
+      <div class="label-with-value">
+        <span>Festnetz:</span>
+        <a href="tel:+43 676 4671756">+43 676 4671756</a>
+      </div>
+    </div>
+  </div>
 </template>
