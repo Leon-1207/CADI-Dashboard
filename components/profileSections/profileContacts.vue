fe95451665319bcc5985312005983482564c592a
Leon Mainusch <leon.mainusch@web.de>
2023-08-30 16:34:16 +0200

    NEW: whatsapp button

--- components/profileSections/profileContacts.vue
+++ components/profileSections/profileContacts.vue
@@ -8,10 +8,10 @@
     <div class="profile-row extend-to-2-columns-420px">
       <div class="label-with-value">
         <span>Mobil:</span>
-        <div>
+        <div class="content-side-by-side">
           <a class="link" href="tel:+4368181246640">+4368181246640</a>
           <a
-            class="basic-button"
+            class="basic-button square-button small"
             href="https://wa.me/XXXXXXXXXXX"
             target="_blank"
           >
