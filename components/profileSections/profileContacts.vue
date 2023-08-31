633bb9872e5d2b0a156805f2605fb655b34c865b
Leon Mainusch <leon.mainusch@web.de>
2023-08-31 14:11:22 +0200

    NEW: profile contacts edit

--- components/profileSections/profileContacts.vue
+++ components/profileSections/profileContacts.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="profile-rows small-gap">
     <!-- 1. Contact -->
-    <div class="label-with-value">
-      <span>Erziehungsberechtigter:</span>
-      <span>Max Mustermann</span>
-    </div>
-    <div class="profile-rows extend-to-2-columns-420px">
+    <div class="profile-rows small-gap extend-to-2-columns-420px">
       <div class="label-with-value">
-        <span>Mobil:</span>
-        <div class="content-side-by-side">
+        <label for="contact-1-name">Erziehungsberechtigter:</label>
+        <input
+          v-if="edit"
+          type="text"
+          name="contact-1-name"
+          class="dashboard-input"
+          id="contact-1-name"
+        />
+        <span v-else>Max Mustermann</span>
+      </div>
+    </div>
+    <div class="profile-rows small-gap extend-to-2-columns-420px">
+      <div class="label-with-value">
+        <label for="contact-1-phone">Mobil:</label>
+        <input
+          v-if="edit"
+          type="tel"
+          name="phone"
+          class="dashboard-input"
+          id="contact-1-phone"
+          pattern="[+]{1}[0-9]{11,14}"
+        />
+        <div v-else class="content-side-by-side">
           <a class="link" href="tel:+4368181246640">+4368181246640</a>
           <a
             class="basic-button square-button small"
@@ -20,8 +37,16 @@
         </div>
       </div>
       <div class="label-with-value">
-        <span>Festnetz:</span>
-        <a href="tel:+43 676 4671756">+43 676 4671756</a>
+        <label for="contact-1-home-phone">Festnetz:</label>
+        <input
+          v-if="edit"
+          type="tel"
+          name="contact-1-home-phone"
+          class="dashboard-input"
+          id="contact-1-home-phone"
+          pattern="[+]{1}[0-9]{11,14}"
+        />
+        <a v-else href="tel:+43 676 4671756">+43 676 4671756</a>
       </div>
     </div>
 
@@ -29,14 +54,31 @@
     <div class="h-line"></div>
 
     <!-- 2. Contact -->
-    <div class="label-with-value">
-      <span>Zusatzkontakt:</span>
-      <span>Tim Tester</span>
-    </div>
-    <div class="profile-rows extend-to-2-columns-420px">
+    <div class="profile-rows small-gap extend-to-2-columns-420px">
       <div class="label-with-value">
-        <span>Mobil:</span>
-        <div class="content-side-by-side">
+        <label for="contact-2-name">Zusatzkontakt:</label>
+        <input
+          v-if="edit"
+          type="text"
+          name="contact-2-name"
+          class="dashboard-input"
+          id="contact-2-name"
+        />
+        <span v-else>Tim Tester</span>
+      </div>
+    </div>
+    <div class="profile-rows small-gap extend-to-2-columns-420px">
+      <div class="label-with-value">
+        <label for="contact-2-phone">Mobil:</label>
+        <input
+          v-if="edit"
+          type="tel"
+          name="contact-2-home-phone"
+          class="dashboard-input"
+          id="contact-2-home-phone"
+          pattern="[+]{1}[0-9]{11,14}"
+        />
+        <div v-else class="content-side-by-side">
           <a class="link" href="tel:+123456">+123456</a>
           <a
             class="basic-button square-button small"
@@ -48,8 +90,15 @@
         </div>
       </div>
       <div class="label-with-value">
-        <span>Beziehungsebene:</span>
-        <span>Onkel</span>
+        <label for="contact-2-type">Beziehungsebene:</label>
+        <input
+          v-if="edit"
+          type="text"
+          name="contact-2-type"
+          class="dashboard-input"
+          id="contact-2-type"
+        />
+        <span v-else>Onkel</span>
       </div>
     </div>
   </div>
