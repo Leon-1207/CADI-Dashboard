8a9765a958d923a880b645b07915648769c94909
Leon Mainusch <leon.mainusch@web.de>
2023-09-08 14:56:09 +0200

    NEW: format file

--- components/popup.vue
+++ components/popup.vue
@@ -57,10 +57,9 @@ export default {
 
 .popup-grid-container {
   display: grid;
-  grid-template-columns: minmax(var(--popup-margin), 1fr) minmax(
-      10fr,
-      var(--popup-target-width)
-    ) minmax(var(--popup-margin), 1fr);
+  grid-template-columns:
+    minmax(var(--popup-margin), 1fr) minmax(10fr, var(--popup-target-width))
+    minmax(var(--popup-margin), 1fr);
   grid-template-rows: minmax(10vmin, 1fr) auto minmax(10vmin, 1fr);
   width: 100vw;
   min-height: 100vh;
