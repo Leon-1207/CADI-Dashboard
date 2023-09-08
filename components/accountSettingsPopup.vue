4dd7a8bc0903e1cef3c02bbd72f3126d38399566
Leon Mainusch <leon.mainusch@web.de>
2023-09-08 14:31:37 +0200

    NEW: settings popup (WIP)

--- components/accountSettingsPopup.vue
+++ components/accountSettingsPopup.vue
@@ -1,6 +1,17 @@
 <template>
   <popup ref="popup">
-    <div class="popup-body">TODO</div>
+    <div class="popup-body popup-content-padding">
+      <div class="profile-rows">
+        <!-- profile data -->
+        <span class="profile-content-headline">Profildaten</span>
+
+        <!-- gap between profile data and settings -->
+        <div class="h-line"></div>
+
+        <!-- settings -->
+        <span class="profile-content-headline">Einstellungen</span>
+      </div>
+    </div>
   </popup>
 </template>
 
